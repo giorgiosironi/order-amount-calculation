8ed939e0140c6fb626629716f0a138d9d6b4b5fc
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-11-27 21:31:15 +0100

    "Back of an napkin" design exploration

--- library/Slam/Order.php
+++ library/Slam/Order.php
@@ -2,35 +2,45 @@
 
 namespace Slam;
 
-class Order
+interface Payable
+{
+	function calcTotalAmount();
+}
+
+/* I think an interesting exploration could be pushing the total into the status.  */
+interface OrderStatus
+{
+	function confirm();
+	function changedProducts(Payable $payable);
+	function getTotalAmount();
+}
+
+class Order implements Payable
 {
 	private $state;
-
-	private $totalAmount;
 
 	private $products;
 
 	public function __construct()
 	{
-		$this->state = new OrderStatusPending();
+		$this->state = new OrderStatusPending(0);
 	}
 
 	public function setProducts(array $products = array())
 	{
 		$this->products = $products;
+		$this->state = $this->state->changedProducts($this);
+		// OrderStatusPending returns a new instance of itself, with updated price
+		// OrderStatusConfirmed throws an exception or just returns itself (your business rules decide)
 	}
 
 	public function getTotalAmount()
 	{
-		// IMHO, this smells!
-		if (! $this->state->isConfirmed()) {
-			return $this->calcTotalAmount();
-		}
-
-		return $this->totalAmount;
+		return $this->state->getTotalAmount();
 	}
 
-	private function calcTotalAmount()
+	/** This should be in an interface, let's say Payable */
+	public function calcTotalAmount()
 	{
 		/**
 		 * This part is currently much more complex
@@ -38,6 +48,7 @@ class Order
 		 * 	- User type
 		 *	- Shipping address
 		 *	- Current special offers
+	  	.. so I'm assuming it must stay here. That's why I pass the whole object instead of just $this->products to changedProducts()
 		 */
 
 		$total = 0;
@@ -50,7 +61,8 @@ class Order
 
 	public function confirm()
 	{
-		$this->state = new OrderStatusConfirmed();
-		$this->totalAmount = $this->calcTotalAmount();
+		$this->state = $this->state->confirm();
+		// OrderStatusPending returns an OrderStatusConfirmed with the same total
+		// OrderStatusConfirmed throws a SomeErrorException or just ignores the message (your business rules decide)
 	}
 }
